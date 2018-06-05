e680006e1c9c301878fa4dbd339dc3398c3aa858
ngovantinh <ngovantinh1994@gmail.com>
2018-06-05 11:09:11 +0700

    bai tap angular 05/06/2018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyArticleComponent } from './my-article/my-article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyArticleComponent
   ],
   imports: [
     BrowserModule
